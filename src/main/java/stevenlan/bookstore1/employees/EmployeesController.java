d5532f87e6e1436ea3d0d7ef2b6743655e47a7cb
Bluedon890 <10255026ekids178@gmail.com>
2024-05-16 17:57:07 +0800

    0516完成基本增刪改查 補上book 完成postman測試

--- src/main/java/stevenlan/bookstore1/employees/EmployeesController.java
+++ src/main/java/stevenlan/bookstore1/employees/EmployeesController.java
@@ -3,10 +3,14 @@ package stevenlan.bookstore1.employees;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,8 +31,28 @@ public class EmployeesController {
 	}
 
     @PostMapping
-    public void registerNewEmployees(@RequestBody Employees employees){
+    public void registerNewEmployees(
+        @RequestBody Employees employees){
         employeesService.addNewEmployees(employees);
+    }
+
+    @DeleteMapping(path = "{employeesId}")
+    public void deleteEmployees(
+        @PathVariable("employeesId") Long employeesId){
+        employeesService.deleteEmployees(employeesId);
+    }
+
+    @PutMapping(path = "{employeesId}")
+    public void updateEmployees(
+        @PathVariable("employeesId") Long employeesId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String account,
+        @RequestParam(required = false) String password,
+        @RequestParam(required = false) String email,
+        @RequestParam(required = false) String phoneNumber){
+            employeesService.updateEmployees(employeesId,name,account,password,email,phoneNumber);
+
+
     }
 
 }
--- src/main/java/stevenlan/bookstore1/employees/EmployeesService.java
+++ src/main/java/stevenlan/bookstore1/employees/EmployeesService.java
@@ -1,10 +1,14 @@
 package stevenlan.bookstore1.employees;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class EmployeesService {
@@ -30,5 +34,36 @@ public class EmployeesService {
             throw new IllegalStateException("account taken");
         }
         employeesRepository.save(employees);
-    };
+    }
+
+    public void deleteEmployees(Long employeesId){
+        boolean exists = employeesRepository.existsById(employeesId);
+        if(!exists){
+            throw new IllegalStateException("this id does not exists");
+        }
+        employeesRepository.deleteById(employeesId);
+    }
+
+    @Transactional
+    public void updateEmployees(
+        Long employeesId, String name, String account, String password, String email, String phoneNumber){
+            Employees employees = employeesRepository.findById(employeesId)
+            .orElseThrow(() -> new IllegalStateException("this id does not exists"));
+
+            if(name != null && name.length() > 0 && !Objects.equals(employees.getName(), name)){
+                employees.setName(name);
+            }
+            if(password != null && password.length() > 0 && !Objects.equals(employees.getPassword(), password)){
+                employees.setPassword(password);
+            }
+            if(email != null && email.length() > 0 && !Objects.equals(employees.getEmail(), email)){
+                employees.setEmail(email);
+            }
+            if(phoneNumber != null && phoneNumber.length() > 0 && !Objects.equals(employees.getPhoneNumber(), phoneNumber)){
+                employees.setPhoneNumber(phoneNumber);
+            }
+            if(account != null && account.length() > 0 && !Objects.equals(employees.getAccount(), account)){
+                employees.setAccount(account);
+            }
+        }
 }
